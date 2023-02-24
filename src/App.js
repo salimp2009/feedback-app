6a6aa87c5d1ed083cabbc2f36234d6972e2a95df
salimp2009@gmail.com <salimp2009@gmail.com>
2023-02-25 01:17:54 +0300

    started adding elements, added index.css (not properly designed yet)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 function App() {
-  return <h1>Feedback App component</h1>;
+  return (
+    <div className="container">
+      <h1>Feedback App component</h1>
+    </div>
+  );
 }
 
 export default App;
