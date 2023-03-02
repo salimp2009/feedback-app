326c2b6afbe69c37b6efc3bdc7b1cef8d7b202be
salimp2009@gmail.com <salimp2009@gmail.com>
2023-03-03 00:52:50 +0300

    added PropType to FeedbackItem and List, added FaTimes icon from react-icon added to FeedBack item

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,13 +1,21 @@
-// import React from "react";
+import { FaTimes } from "react-icons/fa";
+import PropTypes from "prop-types";
 import Card from "./shared/Card";
 
 function FeedbackItem({ item }) {
   return (
     <Card /* reverse={true} */>
       <div className="num-display">{item.rating}</div>
+      <button onClick={() => console.log("testing close")} className="close">
+        <FaTimes color="purple" />
+      </button>
       <div className="text-display">{item.text}</div>
     </Card>
   );
 }
+
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired,
+};
 
 export default FeedbackItem;
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import PropTypes from "prop-types";
 import FeedbackItem from "./FeedbackItem";
 
 function FeedbackList({ feedback }) {
@@ -14,5 +14,15 @@ function FeedbackList({ feedback }) {
     </div>
   );
 }
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      rating: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+    })
+  ),
+};
 
 export default FeedbackList;
