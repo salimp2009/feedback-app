eb0cfcd1c45317b384c19d81495bcdbca7d66f74
salimp2009@gmail.com <salimp2009@gmail.com>
2023-03-20 22:30:16 +0300

    added examples in Post.jsx how to use Navigate, useNavigate to conditionally link to a route or using button with an onClick event

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,14 +1,29 @@
 // Not used for example purposes
 
-import { useParams } from "react-router-dom";
+import { Navigate, useNavigate /* , useParams */ } from "react-router-dom";
 
 function Post() {
-  const params = useParams();
+  // const params = useParams();
+  const status = 200;
+
+  const navigate = useNavigate();
+
+  if (status === 404) {
+    return <Navigate to="/not found" />;
+  }
+
+  const onClickHandler = () => {
+    navigate("/");
+  };
 
   return (
     <div className="posts">
-      <h1>Post {params.id}</h1>
-      <p>Name: {params.name}</p>
+      <h1>Post</h1>
+      <button type="submit" onClick={onClickHandler}>
+        Navigate
+      </button>
+      {/* <h1>Post {params.id}</h1> */}
+      {/* <p>Name: {params.name}</p> */}
     </div>
   );
 }
