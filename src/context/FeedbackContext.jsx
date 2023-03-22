1f94e90306ef4251a4785cda99c4b3c4fa06f99c
salimp2009@gmail.com <salimp2009@gmail.com>
2023-03-22 12:17:48 +0300

    added useEffect when the feedbackEdit changes we get the feedbackEdit from Context and sets the related states; enable button, set text from the Context; did the same also RatingSelect component

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -47,7 +47,13 @@ export const FeedbackProvider = ({ children }) => {
 
   return (
     <FeedbackContext.Provider
-      value={{ feedback, deleteFeedback, addFeedback, editFeedback }}
+      value={{
+        feedback,
+        deleteFeedback,
+        addFeedback,
+        editFeedback,
+        feedbackEdit,
+      }}
     >
       {children}
     </FeedbackContext.Provider>
