ba21f6bb09ec0fe559aa32c0e7d09c0d83402386
salimp2009@gmail.com <salimp2009@gmail.com>
2023-03-29 21:29:04 +0300

    added a spinner.git under /components/assets and used in a Spinner.jsx and called FeedbackList conditionaly if isLoading true

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,31 +1,46 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { v4 as uuid4 } from "uuid";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true);
   const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: "This is feedback item 1",
-      rating: 9,
-    },
-    {
-      id: 2,
-      text: "This is feedback item 2",
-      rating: 10,
-    },
-    {
-      id: 3,
-      text: "This is feedback item 3",
-      rating: 8,
-    },
+    // {
+    //   id: 1,
+    //   text: "This is feedback item 1",
+    //   rating: 9,
+    // },
+    // {
+    //   id: 2,
+    //   text: "This is feedback item 2",
+    //   rating: 10,
+    // },
+    // {
+    //   id: 3,
+    //   text: "This is feedback item 3",
+    //   rating: 8,
+    // },
   ]);
 
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   });
+
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+
+  const fetchFeedback = async () => {
+    const response = await fetch(
+      `http://localhost:5000/feedback?sort=id&_order=desc`
+    );
+    const data = await response.json();
+    console.log(data);
+    setFeedback(data);
+    setIsLoading(false);
+  };
 
   const updateFeedback = (id, updItem) => {
     setFeedback(
@@ -67,6 +82,7 @@ export const FeedbackProvider = ({ children }) => {
       value={{
         feedback,
         feedbackEdit,
+        isLoading,
         deleteFeedback,
         addFeedback,
         editFeedback,
